ae36129d1b3758403c7dec4da401ca804c6f60c6
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-12 00:04:31 +0100

    feat: add delete order

--- routes/api/v1/shoes.js
+++ routes/api/v1/shoes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const shoeController = require('../../../controllers/api/v1/shoeController');
-const authenticate = require('../../../middlewares/authenticate');
 
 // GET all shoe orders
 router.get('/', shoeController.getShoeOrders);
+
 // POST a new shoe order
 router.post('/', shoeController.createShoeOrder);
+
 // GET a shoe order by id
 router.get('/:id', shoeController.getShoeById);
-// DELETE a shoe order by id, admin access required
-router.delete('/:id', authenticate, shoeController.deleteShoeOrder);
+
+// DELETE a shoe order by id
+router.delete('/:id', shoeController.deleteShoeOrder);
 
 module.exports = router;
