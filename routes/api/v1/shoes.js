f0db7d926ef07a22affe2423950b3dc83890f2a6
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-11 23:38:22 +0100

    feat: add delete function with authentication

--- routes/api/v1/shoes.js
+++ routes/api/v1/shoes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const shoeController = require('../../../controllers/api/v1/shoeController');
+const authenticate = require('../../../middlewares/authenticate');
 
 // GET all shoe orders
 router.get('/', shoeController.getShoeOrders);
@@ -8,5 +9,7 @@ router.get('/', shoeController.getShoeOrders);
 router.post('/', shoeController.createShoeOrder);
 // GET a shoe order by id
 router.get('/:id', shoeController.getShoeById);
+// DELETE a shoe order by id, admin access required
+router.delete('/:id', authenticate, shoeController.deleteShoeOrder);
 
 module.exports = router;
