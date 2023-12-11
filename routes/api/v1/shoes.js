0a98a403fbc8de15c41c9360a5aabde3b57a716a
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-11 22:25:15 +0100

    chore: remove non-existing functions

--- routes/api/v1/shoes.js
+++ routes/api/v1/shoes.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const shoeController = require('../../../controllers/api/v1/shoeController');
 
-router.post('/', shoeController.addShoeOrder);
-router.delete('/:id', shoeController.deleteShoeOrder);
-router.put('/:id', shoeController.updateShoeOrder);
-router.get('/:id', shoeController.getShoeDetails);
-router.get('/', shoeController.getAllShoeOrders);
+router.get('/', shoeController.getShoeOrders);
 
 module.exports = router;
