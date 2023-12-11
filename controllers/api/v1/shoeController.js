811a52f380eb547d309182b50edaeb417b165b8e
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-11 22:32:44 +0100

    feat: add function to post orders

--- controllers/api/v1/shoeController.js
+++ controllers/api/v1/shoeController.js
@@ -1,5 +1,6 @@
 const Shoe = require('../../../models/Shoe');
 
+// get all shoe orders
 const getShoeOrders = async (req, res) => {
     try {
         const shoeOrders = await Shoe.find();
@@ -17,6 +18,47 @@ const getShoeOrders = async (req, res) => {
     }
 };
 
+// create a new shoe order
+const createShoeOrder = async (req, res) => {
+    try {
+        const {
+            color,
+            size,
+            lacesColor,
+            soleColor,
+            logoColor,
+            pattern,
+            contactInfo
+        } = req.body;
+
+        // create a new shoe order with provided data
+        const newShoeOrder = new Shoe({
+            color,
+            size,
+            lacesColor,
+            soleColor,
+            logoColor,
+            pattern,
+            contactInfo
+        });
+
+        // save the shoe order to the database
+        const savedShoeOrder = await newShoeOrder.save();
+
+        res.status(201).json({
+            status: 'success',
+            message: 'Shoe order created successfully',
+            data: {
+                shoeOrder: savedShoeOrder,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+    }
+};
+
 module.exports = {
     getShoeOrders,
+    createShoeOrder,
 };
--- routes/api/v1/shoes.js
+++ routes/api/v1/shoes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const shoeController = require('../../../controllers/api/v1/shoeController');
 
 router.get('/', shoeController.getShoeOrders);
+router.post('/', shoeController.createShoeOrder);
 
 module.exports = router;
