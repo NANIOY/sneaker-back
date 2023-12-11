296b198bdcfbb3d29b8dd00d59acff5cc379e52f
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-11 23:01:12 +0100

    feat: add date created field

--- models/Shoe.js
+++ models/Shoe.js
@@ -29,6 +29,10 @@ const ShoeSchema = new Schema({
         type: String,
         required: true,
     },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 const Shoe = mongoose.model('Shoe', ShoeSchema);
