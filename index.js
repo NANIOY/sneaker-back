acd0f672b4d0f9f6447de12061c0aaba83fbe8e8
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-12 17:29:12 +0100

    feat: add comments + restructure

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
+// import dependencies
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+// create Express app
 const app = express();
 const port = process.env.PORT || 3000;
 
-const cors = require('cors');
+// middleware
 app.use(cors());
+app.use(express.json());
 
+// connect to MongoDB
 mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
+// import route handlers
 const shoeRoutes = require('./routes/api/v1/shoes');
 const userRoutes = require('./routes/api/v1/users');
 const authRoutes = require('./routes/api/v1/auth');
 
-app.use(express.json());
-
+// use route handlers for different parts of the application
 app.use('/api/v1/shoes', shoeRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/auth', authRoutes);
 
+// start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`server is running on port ${port}`);
 });
