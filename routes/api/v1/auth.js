b5154fed49ab19d956d474559e70187f12e1fce7
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-12 17:14:15 +0100

    feat: logout by destroying current token

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require('../../../controllers/api/v1/authController');
 
 router.post('/login', authController.login);
+router.post('/logout', authController.logout);
 
 module.exports = router;
