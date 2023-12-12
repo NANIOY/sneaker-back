97fbc59e21db0922c617245b56fcf46b680594ef
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-12 15:00:39 +0100

    feat: require authentication when deleting user

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../../controllers/api/v1/userController');
+const authenticate = require('../../../middlewares/authenticate');
 
+// create new user
 router.post('/', userController.createUser);
+
+// get all users
 router.get('/', userController.getAllUsers);
+
+// delete user (admin only)
+router.delete('/:userId', authenticate, userController.deleteUser);
 
 module.exports = router;
