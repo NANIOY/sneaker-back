5d78ecab7c03168bfd93fee979fe603f3faea1a1
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-12 13:04:36 +0100

    feat: get all users

--- controllers/api/v1/userController.js
+++ controllers/api/v1/userController.js
@@ -20,4 +20,17 @@ const createUser = async (req, res) => {
     }
 };
 
-module.exports = { createUser };
+const getAllUsers = async (res) => {
+    try {
+        const users = await User.find({}, { password: 0 }); // exclude password from response
+        res.status(200).json({ users });
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+module.exports = { 
+    createUser,
+    getAllUsers,
+};
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../../../controllers/api/v1/userController');
 
 router.post('/', userController.createUser);
+router.get('/', userController.getAllUsers);
 
 module.exports = router;
