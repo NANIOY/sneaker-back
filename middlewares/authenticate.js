0fc1bcf6048664447ff4051b3764aba192a4f84e
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-12 14:58:49 +0100

    feat: create authentication middleware

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,26 +1,24 @@
-// // middlewares/authenticate.js
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-// const authenticate = (req, res, next) => {
-//     const token = req.headers.authorization;
+const authenticate = (req, res, next) => {
+    const token = req.headers.authorization;
 
-//     if (!token) {
-//         return res.status(401).json({ status: 'error', message: 'Unauthorized: Missing token' });
-//     }
+    if (!token) {
+        return res.status(401).json({ status: 'error', message: 'Unauthorized: Missing token' });
+    }
 
-//     try {
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-//         // Check if the user is an admin
-//         if (!decoded.isAdmin) {
-//             return res.status(403).json({ status: 'error', message: 'Forbidden: Admin access required' });
-//         }
+        req.user = {
+            userId: decoded.userId,
+            isAdmin: decoded.isAdmin || false,
+        };
 
-//         req.user = decoded;
-//         next();
-//     } catch (error) {
-//         return res.status(401).json({ status: 'error', message: 'Unauthorized: Invalid token' });
-//     }
-// };
+        next();
+    } catch (error) {
+        return res.status(401).json({ status: 'error', message: 'Unauthorized: Invalid token' });
+    }
+};
 
-// module.exports = authenticate;
+module.exports = authenticate;
