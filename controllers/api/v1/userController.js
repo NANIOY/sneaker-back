9bd72c10b2ad9b03c5bc2d59f83a579016e95499
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-12 12:57:11 +0100

    feat: add email + all fields have to be filled in

--- controllers/api/v1/userController.js
+++ controllers/api/v1/userController.js
@@ -2,18 +2,17 @@ const User = require('../../../models/User');
 
 const createUser = async (req, res) => {
     try {
-        const { username, password } = req.body;
+        const { username, email, password } = req.body;
 
         // check if user already exists
-        const existingUser = await User.findOne({ username });
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
         if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
+            return res.status(400).json({ message: 'User with the same username or email already exists' });
         }
 
         // create new user
-        const newUser = new User({ username, password });
+        const newUser = new User({ username, email, password });
         await newUser.save();
-
         res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
         console.error('Error creating user:', error);
