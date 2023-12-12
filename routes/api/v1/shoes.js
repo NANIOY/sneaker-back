af7276b4c69514282b6a27a840995d60f020f621
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-12 15:02:24 +0100

    chore: remove authentication

--- routes/api/v1/shoes.js
+++ routes/api/v1/shoes.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const shoeController = require('../../../controllers/api/v1/shoeController');
-// const authenticate = require('../../../middlewares/authenticate');
-// const authController = require('../../../controllers/api/v1/authController');
-
-// Auth routes
-// router.post('/auth/login', authController.login);
-// router.post('/auth/register', authController.register);
 
 // GET all shoe orders
 router.get('/', shoeController.getShoeOrders);
