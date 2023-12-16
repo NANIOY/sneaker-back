3794815f071b84d79aef9782df6dd70000d07e2e
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-16 16:56:29 +0100

    Update live.js

--- primus/live.js
+++ primus/live.js
@@ -1,23 +1,33 @@
+// Import the required modules
+const Primus = require("primus");
+const { createShoeOrder } = require('../controllers/api/v1/shoeController');
+
 module.exports.go = (server) => {
-    const Primus = require("primus");
+    // Create a new Primus instance
     const primus = new Primus(server, {
         transformer: "websockets",
     });
 
-    spark.on('data', async (data) => {
-        console.log("data (❁´◡`❁)", data);
-    
-        try {
-            // call function to create new order
-            const newOrder = await createShoeOrder(data);
-    
-            // send new order to all connected clients
-            primus.write({
-                type: 'new_order',
-                data: newOrder,
-            });
-        } catch (error) {
-            console.error('Error creating new order:', error);
-        }
+    // Handle incoming WebSocket connections
+    primus.on('connection', (spark) => {
+        console.log('connected ༼ つ ◕_◕ ༽つ');
+
+        // Handle incoming data from clients
+        spark.on('data', async (data) => {
+            console.log("data (❁´◡`❁)", data);
+
+            try {
+                // Call function to create new order
+                const newOrder = await createShoeOrder(data);
+
+                // Send new order to all connected clients
+                primus.write({
+                    type: 'new_order',
+                    data: newOrder.data.shoeOrder,
+                });
+            } catch (error) {
+                console.error('Error creating new order:', error);
+            }
+        });
     });
 };
